78044e3a738de2f17cb3f401b5387f3d7ba32507
Igor <GI_Something@protonmail.com>
2020-10-20 11:56:08 +0300

    feat: Save results to a file after translation

--- translator/controller.py
+++ translator/controller.py
@@ -1,7 +1,9 @@
 def run(view, model):
     mode, text = view.get_mode(model.supported_languages), view.get_text()
-    for from_to_lang in mode:
+
+    for i, from_to_lang in enumerate(mode):
         response = model.get_page(from_to_lang[0], from_to_lang[1], text)
         translations = model.get_translations(response.content)
         examples = model.get_examples(response.content)
         view.show_results(translations, examples, from_to_lang)
+        view.save_results_to_file(translations, examples, from_to_lang, f"{text[:80]}.txt", append=bool(i))
--- translator/view.py
+++ translator/view.py
@@ -1,3 +1,4 @@
+from sys import stdout
 from itertools import product
 
 
@@ -22,17 +23,26 @@ def show_header(languages):
         print(f"{k}. {v}")
 
 
-def show_translations(translations, to_lang):
-    print(f"\n{to_lang} translations:", *translations, sep="\n")
+def show_translations(translations, to_lang, out=stdout):
+    print(f"\n{to_lang} translations:", *translations, sep="\n", file=out)
 
 
-def show_examples(examples, to_lang):
-    print(f"\n{to_lang} examples:")
+def show_examples(examples, to_lang, out=stdout):
+    print(f"\n{to_lang} examples:", file=out)
     for i in range(0, len(examples), 2):
-        print(f"{examples[i]}:")
-        print(f"{examples[i + 1]}\n")
+        print(f"{examples[i]}:", file=out)
+        print(f"{examples[i + 1]}\n", file=out)
 
 
 def show_results(translations, examples, mode):
     show_translations(translations, mode[1])
     show_examples(examples, mode[1])
+
+
+def save_results_to_file(translations, examples, from_to_lang, file_path, append=True):
+    if not append:  # clear file
+        with open(file_path, "w", encoding="utf-8"):
+            pass
+    with open(file_path, "a", encoding="utf-8") as out_file:
+        show_translations(translations, from_to_lang[1], out=out_file)
+        show_examples(examples, from_to_lang[1], out=out_file)
